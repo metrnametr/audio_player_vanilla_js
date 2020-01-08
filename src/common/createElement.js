00c5439156ebafe17254b131c9272cfe952918d2
metrnametr <patronius1515@gmail.com>
2020-01-09 01:16:59 +0200

    prettier fixup file

--- src/common/createElement.js
+++ src/common/createElement.js
@@ -2,19 +2,22 @@ import { isEmpty, isArray, isString, keys, forEach } from 'lodash';
 
 function createElement(element, classNames, attributes, children) {
   const el = document.createElement(element);
-  if (!isEmpty(classNames)) isArray(classNames) ? forEach(classNames, (className) => el.classList.add(className)) : el.classList.add(classNames);
+  if (!isEmpty(classNames))
+    isArray(classNames)
+      ? forEach(classNames, (className) => el.classList.add(className))
+      : el.classList.add(classNames);
 
   if (!isEmpty(attributes)) {
     if (attributes.styles) {
-      forEach(keys(attributes.styles), (style) => el.style[style] = attributes.styles[style]);
+      forEach(keys(attributes.styles), (style) => (el.style[style] = attributes.styles[style]));
       delete attributes.styles;
     }
-    forEach(keys(attributes), (attribute) => el[attribute] = attributes[attribute]);
+    forEach(keys(attributes), (attribute) => (el[attribute] = attributes[attribute]));
   }
 
   if (!isString(children) && children !== undefined) {
     isArray(children) ? forEach(children, (child) => el.appendChild(child)) : el.appendChild(children);
-  } 
+  }
   if (isString(children) && children !== undefined) {
     el.innerText = children;
   }
